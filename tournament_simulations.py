323bf5a871251b6b892f4094b3ca142d62ad6d48
zculp6 <zachculp6@gmail.com>
2025-03-14 15:24:38 -0500

    Updated tournament simulations app

--- tournament_simulations.py
+++ tournament_simulations.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.graph_objects as go
-import math
 
 # Streamlit app layout
 st.title('NCAA Tournament Simulation with Interactive Bracket')
